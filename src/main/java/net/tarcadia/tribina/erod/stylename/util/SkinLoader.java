ecc0b8a4b9ab5ae6dd73230613678467b6a8a6f6
Tarcadia <tarcadia@qq.com>
2022-03-22 22:59:57 +0800

    better looking

--- src/main/java/net/tarcadia/tribina/erod/stylename/util/SkinLoader.java
+++ src/main/java/net/tarcadia/tribina/erod/stylename/util/SkinLoader.java
@@ -114,9 +114,9 @@ public class SkinLoader {
             PacketContainer packet;
             if (
                     this.sn.isFunctionEnabled() &&
-                            ((packet = event.getPacket()) != null) &&
-                            (packet.getType().equals(PacketType.Play.Server.PLAYER_INFO)) &&
-                            (packet.getPlayerInfoAction().read(0).equals(EnumWrappers.PlayerInfoAction.ADD_PLAYER))
+                    ((packet = event.getPacket()) != null) &&
+                    (packet.getType().equals(PacketType.Play.Server.PLAYER_INFO)) &&
+                    (packet.getPlayerInfoAction().read(0).equals(EnumWrappers.PlayerInfoAction.ADD_PLAYER))
             ) {
                 var nPIDList = new LinkedList<PlayerInfoData>();
                 for (var rPID : packet.getPlayerInfoDataLists().read(0)) {
