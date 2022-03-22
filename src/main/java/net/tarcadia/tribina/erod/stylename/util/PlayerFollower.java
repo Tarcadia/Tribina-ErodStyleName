4239c35b3c6bce70ef9b1afe1fd742f924e51e64
Tarcadia <tarcadia@qq.com>
2022-03-22 19:03:08 +0800

    debug

--- src/main/java/net/tarcadia/tribina/erod/stylename/util/PlayerFollower.java
+++ src/main/java/net/tarcadia/tribina/erod/stylename/util/PlayerFollower.java
@@ -79,7 +79,9 @@ class PlayerFollowerViewer extends PacketAdapter {
         ProtocolLibrary.getProtocolManager().addPacketListener(this);
     }
 
-    public void end() {
+    synchronized public void end() {
+        var playerViewedByList = new LinkedList<>(playerViewedBy);
+        for (var viewer : playerViewedByList) this.viewOut(viewer);
         ProtocolLibrary.getProtocolManager().removePacketListener(this);
     }
 
@@ -341,34 +343,30 @@ public class PlayerFollower extends BukkitRunnable {
 
     @Override
     public void run() {
-        if (this.player.isOnline()) {
-            var nowGM = this.player.getGameMode();
-            if (this.lastGM == null) this.follower.canViewFollower(!nowGM.equals(GameMode.SPECTATOR));
-            else if (this.lastGM.equals(GameMode.SPECTATOR) && !nowGM.equals(GameMode.SPECTATOR))
-                this.follower.canViewFollower(true);
-            else if (!this.lastGM.equals(GameMode.SPECTATOR) && nowGM.equals(GameMode.SPECTATOR))
-                this.follower.canViewFollower(false);
-            var nowLoc = this.player.getLocation().clone();
-            var nowSneaking = this.player.isSneaking();
-            boolean ifMove = (
-                    this.lastSneaking != nowSneaking ||
-                    this.lastLoc == null ||
-                    this.lastLoc.getX() != nowLoc.getX() ||
-                    this.lastLoc.getY() != nowLoc.getY() ||
-                    this.lastLoc.getZ() != nowLoc.getZ()
-            );
-            boolean ifMeta = (
-                    this.lastSneaking != nowSneaking
-            );
+        var nowGM = this.player.getGameMode();
+        if (this.lastGM == null) this.follower.canViewFollower(!nowGM.equals(GameMode.SPECTATOR));
+        else if (this.lastGM.equals(GameMode.SPECTATOR) && !nowGM.equals(GameMode.SPECTATOR))
+            this.follower.canViewFollower(true);
+        else if (!this.lastGM.equals(GameMode.SPECTATOR) && nowGM.equals(GameMode.SPECTATOR))
+            this.follower.canViewFollower(false);
+        var nowLoc = this.player.getLocation().clone();
+        var nowSneaking = this.player.isSneaking();
+        boolean ifMove = (
+                this.lastSneaking != nowSneaking ||
+                this.lastLoc == null ||
+                this.lastLoc.getX() != nowLoc.getX() ||
+                this.lastLoc.getY() != nowLoc.getY() ||
+                this.lastLoc.getZ() != nowLoc.getZ()
+        );
+        boolean ifMeta = (
+                this.lastSneaking != nowSneaking
+        );
 
-            if (ifMove && ifMeta) this.follower.viewMoveMetaUpdateFollowerAll();
-            else if (ifMove) this.follower.viewMoveUpdateFollowerAll();
-            else if (ifMeta) this.follower.viewMetaUpdateFollowerAll();
-            this.lastLoc = nowLoc;
-            this.lastGM = nowGM;
-            this.lastSneaking = nowSneaking;
-        } else {
-            this.end();
-        }
+        if (ifMove && ifMeta) this.follower.viewMoveMetaUpdateFollowerAll();
+        else if (ifMove) this.follower.viewMoveUpdateFollowerAll();
+        else if (ifMeta) this.follower.viewMetaUpdateFollowerAll();
+        this.lastLoc = nowLoc;
+        this.lastGM = nowGM;
+        this.lastSneaking = nowSneaking;
     }
 }
